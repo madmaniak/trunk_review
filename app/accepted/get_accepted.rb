48d4866130e208da73defc8cf0ea5fb07c3adba2
Marek Piasecki <mad@maniak.pro>
2017-10-05 08:55:32 +0200

    change order of accepted

--- app/accepted/get_accepted.rb
+++ app/accepted/get_accepted.rb
@@ -3,7 +3,7 @@ Getter __FILE__ do
   setup \
     base: :commits,
     fields: [:hash, :time, :status, :message],
-    order: [ [field('time').cast(:timestamp), false] ],
+    order: [ [field('time').cast(:timestamp), true] ],
     limit: 10,
 
     scope: ->(ds, params){ ds.where(field('status')=>'accepted') }
